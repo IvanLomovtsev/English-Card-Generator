aa21dc0f2942ec2093a90e9a284f7578ad692897
ivanlomovtsev <i.d.lomovtsev@gmail.ru>
2022-09-01 22:11:56 +0500

    add: create card-generator

--- js/script.js
+++ js/script.js
@@ -1,3 +1,27 @@
+let word = document.querySelector('.word');
+let transcription = document.querySelector('.transcription');
+let translate = document.querySelector('.translate');
+
+function getNewWord() {
+  const dectinary = 'json/dectinary.json';
+  fetch(dectinary)
+  .then(res => res.json())
+  .then(data => { 
+    for (let i=0; i<data.length; i++){
+      console.log(data[0]['word']);
+      word.textContent = data[i]['word'];
+      transcription.textContent = data[i]['transcription'];
+      translate.textContent = data[i]['translate'];
+      convert();
+    }
+  })
+}
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 function convert() {
     html2canvas(document.querySelector("#capture")).then(canvas => {
                  // Сохраняем содержимое холста как файл и скачиваем
@@ -6,4 +30,4 @@ function convert() {
       });
     });
   }
-  convert();
+getNewWord();
